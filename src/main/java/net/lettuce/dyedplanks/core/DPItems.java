c2be3ba65711fd6263a48dc1e9d1f967b60456d4
BuildTools <unconfigured@null.spigotmc.org>
2024-10-23 11:39:32 -0400

    Initial commit

---
+++ src/main/java/net/lettuce/dyedplanks/core/DPItems.java
@@ -0,0 +1,13 @@
+package net.lettuce.dyedplanks.core;
+
+import net.lettuce.dyedplanks.LettuceDyedPlanks;
+import net.minecraft.world.item.Item;
+import net.minecraftforge.registries.DeferredRegister;
+import net.minecraftforge.registries.ForgeRegistries;
+
+public class DPItems {
+    public static final DeferredRegister<Item> ITEMS = DeferredRegister.create(ForgeRegistries.ITEMS, LettuceDyedPlanks.MOD_ID);
+
+
+
+}
